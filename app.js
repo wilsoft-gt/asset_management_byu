e0162052d9c90a5f665cf809adeae2f936e93b95
wilsoft-gt <wilson.romero1992@gmail.com>
2025-04-08 02:40:30 -0600

    Changed cors

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const views = require("./src/view/index.js")
 const cors = require("cors")
 
 
-app.use(express.json())
 app.use(cors())
+app.use(express.json())
 app.use(views)
 app.listen(port = process.env.PORT || 3000, () => {
   console.log(`App running on port ${port}`)
