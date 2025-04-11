f8df5ca79096c5d70ea2aba24d7fc77574acccc0
wilsoft-gt <wilson.romero1992@gmail.com>
2025-04-10 23:42:01 -0600

    Added home page for backend

--- src/utils/funcUtils.js
+++ src/utils/funcUtils.js
@@ -1,4 +1,49 @@
+
 const funcUtils = {}
+
+
+funcUtils.renderHome = () => {
+  return /*html*/`
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Asset Management System Backend</title>
+    <style>
+      * {
+        box-sizing: border-box;
+      }
+
+      body {
+        margin: 0;
+        height: 100vh;
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        background-color: #eee;
+      }
+      main {
+        background-color: white;
+        border: 1px solid #ccc;
+        border-radius: 5px;
+        padding: 4em;
+        box-shadow: 2px 5px 5px #ccc;
+      }
+    </style>
+  </head>
+  <body>
+    <main>
+      <section>
+        <h1>This site is just a template</h1>
+        <p>To view the actual website visit <a href="https://wilsoft-gt.github.io/asset-management-front-byu/">Asset Management System</a></p>
+      </section>
+    </main>
+  </body>
+  </html>
+  `
+}
 
 
 module.exports = funcUtils
--- src/view/index.js
+++ src/view/index.js
@@ -4,6 +4,7 @@ const project = require("./projectViews")
 const user = require("./userView")
 const asset = require("./assetViews")
 const announcement = require("./announcemenentView")
+const funcTools = require("../utils/funcUtils")
 
 router.use("/auth", auth)
 router.use("/projects", project)
@@ -11,7 +12,7 @@ router.use("/users", user)
 router.use("/assets", asset)
 router.use("/announcements", announcement)
 router.get("/", (req, res) => {
-  res.status(200).send("<h1>Welcome to the site!</h1>")
+  res.status(200).send(funcTools.renderHome())
 })
 
 module.exports = router
